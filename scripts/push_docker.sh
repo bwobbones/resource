3433151980a204149eb8993ef39aeeaa9097aba1
Greg Lucas-Smith <greg@resourcefulsoftware.com.au>
2016-08-16 08:47:18 +0800

    #94 deploy to ecs

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -10,3 +10,6 @@ docker build -t bwobsandbox/resource-app .
 # push to dockerhub
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker push bwobsandbox/resource-app
+
+# deploy
+ecs-deploy -k $AWS_KEY -s $AWS_SECRET -r $AWS_REGION -c resource -n resource -i bwobsandbox/resource-app
