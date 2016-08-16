0e8735c737777069aafee8821469e8d733f55b82
Greg Lucas-Smith <greg@resourcefulsoftware.com.au>
2016-08-16 08:22:01 +0800

    #94 login to docker in the push script

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
+source .env
 
 # build resource dist
 grunt build
-
-# stop the containers
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
 
 # build the flask container
 docker build -t bwobsandbox/resource-app .
 
 # push to dockerhub
-
+docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker push bwobsandbox/resource-app
