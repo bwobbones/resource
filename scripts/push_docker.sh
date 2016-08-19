bd83f462f4d1806d9047113b7f8d2102a84d16ba
Greg Lucas-Smith <greg@resourcefulsoftware.com.au>
2016-08-19 22:58:00 +0800

    #94 change the service name, remove the cpu definition as it is optional

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -12,4 +12,4 @@ docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker push bwobsandbox/resource-app
 
 # deploy
-ecs-deploy -k $AWS_KEY -s $AWS_SECRET -r $AWS_REGION -c resource -n resource -i bwobsandbox/resource-app
+ecs-deploy -k $AWS_KEY -s $AWS_SECRET -r $AWS_REGION -c resource -n ecscompose-service-resource -i bwobsandbox/resource-app
